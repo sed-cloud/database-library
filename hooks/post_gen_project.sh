4f5a898566e8076e2dbd58f8a4f52e3088631ea2
Stephen Davis <stephenedavis17@gmail.com>
2021-09-26 15:04:09 -0400

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -13,28 +13,28 @@ docker-compose up test
 # echo "${GH_PAT}" > ghpat.txt
 # gh auth login --with-token < ghpat.txt
 
-git config --global user.name "Stephen Davis"
-git config --global user.email "stephenedavis17@gmail.com"
+# git config --global user.name "Stephen Davis"
+# git config --global user.email "stephenedavis17@gmail.com"
 
-git init . 
+# git init . 
 # echo "gh repo create ${{cookiecutter.package_name}}"
 # gh repo create ${{cookiecutter.package_name}}
 
 # gh secret set GH_PAT -b"${GH_PAT}"
 # gh secret set PYPI_USERNAME -b"${PYPI_USERNAME}"
 # gh secret set PYPI_PASSWORD -b"${PYPI_PASSWORD}"
-git remote add origin https://github.com/sed-cloud/${{cookiecutter.package_name}}.git
+# git remote add origin https://github.com/sed-cloud/${{cookiecutter.package_name}}.git
 
-pre-commit install
+# pre-commit install
 
-git checkout -b main
+# git checkout -b main
 
 # run this twice to ensure that pre-commit worked
 # this is because precommit may fix things but
 # still mark them as invalid, weird.
-git add . 
-git commit -m "Initial Commit"
-git add . 
-git commit -m "Initial Commit"
+# git add . 
+# git commit -m "Initial Commit"
+# git add . 
+# git commit -m "Initial Commit"
 
-git push --set-upstream origin main
+# git push --set-upstream origin main
