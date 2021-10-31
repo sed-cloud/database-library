fbbaa4b0d6440a756c0bbfab5ba5ae2619dd44f6
Stephen Davis <stephenedavis17@gmail.com>
2021-10-31 13:55:59 -0400

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose run --rm {{cookiecutter.package_name}}-service bash "cd /workarea/{{cookiecutter.package_name}}; alembic init alembic"
+docker-compose run --rm {{cookiecutter.package_name}}-service bash "cd /workarea/; alembic init alembic"
