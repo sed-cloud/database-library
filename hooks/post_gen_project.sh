3324c2b0962dd4c4b5a80cf361ee91c7044ea19e
Stephen Davis <stephenedavis17@gmail.com>
2021-10-31 16:14:17 -0400

    Added alembic.ini modifier

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker-compose run --rm {{cookiecutter.package_name}}-service alembic init alembic
+
+sed -i 's/sqlalchemy.url = driver:\/\/user:pass@localhost\/dbname\/sqlalchemy.url = driver:\/\/docker:docker@localhost\/{{ cookiecutter.database_name }}\/' alembic.ini
