a5681f7842323925bc51f5d374a3ca1e42e0dd62
Stephen Davis <stephenedavis17@gmail.com>
2021-11-07 22:35:41 -0500

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,3 +2,5 @@
 docker-compose run --rm {{cookiecutter.package_name}}-service alembic init alembic
 
 sed -i -e 's/sqlalchemy.url = driver:\/\/user:pass@localhost\/dbname/sqlalchemy.url = postgresql:\/\/docker:docker@{{cookiecutter.package_name}}-postgres-container\/{{ cookiecutter.database_name }}/g' alembic.ini
+sed -i -e 's/from alembic import context/from alembic import context\nfrom {{cookiecutter.package_name}}.database import Base/g' alembic/env.py
+sed -i -e 's/target_metadata = None/target_metadata = Base.metadata/g' alembic/env.py
